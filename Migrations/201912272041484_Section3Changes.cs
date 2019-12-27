ad1336fc10fad5526ff123f581145e2b5cef7729
Patrick Thompson <thompsonpa2014@gmail.com>
2019-12-27 16:02:27 -0500

    Section 3 Changes

---
+++ Migrations/201912272041484_Section3Changes.cs
@@ -0,0 +1,16 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Section3Changes : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
