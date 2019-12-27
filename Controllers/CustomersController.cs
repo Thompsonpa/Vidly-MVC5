49cabb71f7dc056c88411a609fd740892a3bc66b
Patrick Thompson <thompsonpa2014@gmail.com>
2019-12-26 22:19:42 -0500

    inital Code Push Section 1 -2 completed

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers/view
+        public ActionResult view()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Patrick Thompson", Id = 1},
+                new Customer { Name = "Samantha Thompson", Id = 2},
+                new Customer { Name = "Brian Thompson", Id = 3},
+                new Customer { Name = "Leroy Thompson", Id = 4}
+
+            };
+
+
+            var viewModel = new CustomerListViewModel
+            {
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/View
+        public ActionResult view()
+        {
+            var movies = new List<Movies>
+            {
+                new Movies { Name = "Shrek!", Id = 1},
+                new Movies { Name = "Finding Nemo", Id = 2},
+                new Movies { Name = "The Blind Side", Id = 3},
+                new Movies { Name = "Cars", Id = 4},
+                new Movies { Name = "2 Fast 2 Furious", Id = 5},
+            };
+
+            var viewModel = new MovieListViewModel
+            {
+                Movies = movies
+            };
+
+            return View(viewModel);
+        }
+
+        //GET: Edit Movies/Edit/{id}
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        [Route("Movies/Released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+        
+    }
+}
